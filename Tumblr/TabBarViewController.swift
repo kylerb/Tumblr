d9fc7a085ca6ac12f75193403535207de2240b97
Your Name HereKyler <bluekylerblue@gmail.com>
2016-11-01 17:31:39 -0700

    initial commit -- basic setup

---
+++ Tumblr/TabBarViewController.swift
@@ -0,0 +1,85 @@
+//
+//  TabBarViewController.swift
+//  Tumblr
+//
+//  Created by Kyler Blue on 11/1/16.
+//  Copyright © 2016 Kyler Blue. All rights reserved.
+//
+
+import UIKit
+
+var homeViewController: UIViewController!
+var searchViewController: UIViewController!
+var accountViewController: UIViewController!
+var trendingViewController: UIViewController!
+var viewControllers: [UIViewController]!
+var selectedIndex: Int = 0
+
+class TabBarViewController: UIViewController {
+    
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet var buttons: [UIButton]!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+        searchViewController = storyboard.instantiateViewController(withIdentifier: "SearchViewController")
+        accountViewController = storyboard.instantiateViewController(withIdentifier: "AccountViewController")
+        trendingViewController = storyboard.instantiateViewController(withIdentifier: "TrendingViewController")
+        
+        viewControllers = [homeViewController, searchViewController, accountViewController, trendingViewController]
+        
+        buttons[selectedIndex].isSelected = true
+        didPressTab(buttons[selectedIndex])
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBAction func didPressTab(_ sender: UIButton) {
+        let previousIndex = selectedIndex
+        selectedIndex = sender.tag
+        buttons[previousIndex].isSelected = false
+        let previousVC = viewControllers[previousIndex]
+        previousVC.willMove(toParentViewController: nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        sender.isSelected = true
+        let vc = viewControllers[selectedIndex]
+        
+        addChildViewController(vc)
+        
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        vc.didMove(toParentViewController: self)
+        print("Tag: \(selectedIndex)")
+        
+        
+        
+    }
+    
+    
+}
